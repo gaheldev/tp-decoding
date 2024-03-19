d5890fe9702e6ca74e8a756384b386a1a5cc6f02
Gahel <music@gahel.fr>
2024-03-19 17:13:16 +0100

    Use wget to download dataset with loading bar

--- tp/downloader.py
+++ tp/downloader.py
@@ -1,7 +1,7 @@
-from urllib.request import urlretrieve
 from shutil import unpack_archive
 import os
-from os.path import join
+from os.path import join, isfile
+import wget
 
 
 COMP_LINK = 'https://www.bbci.de/competition/download/competition_iv/BCICIV_4_mat.zip'
@@ -12,26 +12,44 @@ EVAL_LINK = 'https://www.bbci.de/competition/iv/results/ds4/true_labels.zip'
 def download_bciciv(target_folder):
     comp_archive = join(target_folder, COMP_LINK.split('/')[-1])
     print(f'Downloading {os.path.basename(comp_archive)}...')
-    retrieve(COMP_LINK, comp_archive)
+    download(COMP_LINK, comp_archive, unsafe=True)
 
     eval_archive = join(target_folder, EVAL_LINK.split('/')[-1])
+    print()
     print(f'Downloading {os.path.basename(eval_archive)}...')
-    retrieve(EVAL_LINK, eval_archive)
+    download(EVAL_LINK, eval_archive, unsafe=True)
 
     unpack_archive(comp_archive, target_folder)
     unpack_archive(eval_archive, target_folder)
 
     os.remove(comp_archive)
     os.remove(eval_archive)
+    print()
     print('Done')
 
 
 
-def retrieve(url, to_file):
+def download(url, out=None, unsafe=False):
+    """ wget wrapper with some safety net
+
+        WARNING: if out exists, it will be removed !!
+    """
     try:
-        urlretrieve(url, to_file)
+        if isfile(out):
+            remove(out)
+        wget.download(url, out=out)
+
+    except KeyboardInterrupt:
+        import sys
+        print()
+        print("Aborting...")
+        sys.exit()
+
     except:
-        # the website's certificate is expired, let's use an unsafe download
+        if not unsafe:
+            raise
+
+        # download even if certificate is invalid
         import ssl
         ssl._create_default_https_context = ssl._create_unverified_context
-        urlretrieve(url, to_file)
+        wget.download(url, out=out)
